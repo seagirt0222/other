5fed592531e804d6c846af2d76f20a5bc5289a93
seagirt0222 <43135436+seagirt0222@users.noreply.github.com>
2020-10-22 15:44:09 +0800

    Update check_docker.sh

--- check_docker.sh
+++ check_docker.sh
@@ -22,22 +22,4 @@ else
    echo "Docker is install success."
    sleep 5
 fi
-function printit(){
-	echo -n "Your choice is "     # 加上 -n 可以不斷行繼續在同一行顯示
-}
 
-echo "This program will print your selection !"
-case ${1} in
-  "one")
-	printit; echo ${1} | tr 'a-z' 'A-Z'  # 將參數做大小寫轉換！
-	;;
-  "two")
-	printit; echo ${1} | tr 'a-z' 'A-Z'
-	;;
-  "three")
-	printit; echo ${1} | tr 'a-z' 'A-Z'
-	;;
-  *)
-	echo "Usage ${0} {one|two|three}"
-	;;
-esac
