af4d5d6cd2aa0749de7bfabaf303c31d9e4d08bd
seagirt0222 <43135436+seagirt0222@users.noreply.github.com>
2020-10-26 10:10:58 +0800

    Update install_telegraf.sh

--- install_telegraf.sh
+++ install_telegraf.sh
@@ -1,11 +1,31 @@
 #/bin/sh
+echo "1.install Telegraf，2.install Filebeat"
+echo -n "Chiose："
+read ANS
 
-cat <<EOF | sudo tee /etc/apt/sources.list.d/influxdata.list
-deb https://repos.influxdata.com/ubuntu bionic stable
-EOF
+case $ANS in
+  1) cat <<EOF | sudo tee /etc/apt/sources.list.d/influxdata.list
+     deb https://repos.influxdata.com/ubuntu bionic stable
+     EOF
 
-sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+     sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 
-sudo apt-get update
-
-sudo apt-get -y install telegraf
+     sudo apt-get update
+     sleep 5
+     sudo apt-get -y install telegraf
+     sleep 5
+     echo "Install telegraf OK";;
+     
+  2) wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+     echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+     sleep 5
+     
+     sudo apt-get install apt-transport-https
+     sudo apt update
+     sleep 5
+     sudo apt install filebeat
+     sleep 5
+     
+     echo "Install Filebeat OK";;
+      *)
+   echo "目前只有2個選項,只能按1,2,的按鍵";;
