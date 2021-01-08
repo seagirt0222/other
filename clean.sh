6337491c16a4ffec3f96479cc022d2d3ce31b62f
seagirt0222 <43135436+seagirt0222@users.noreply.github.com>
2021-01-08 13:51:31 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #keep 10days
-
+# 保留10天
 sudo journalctl --vacuum-time=10
 
+# del log
 sudo rm *.gz
 
+# del log
 sudo rm *.1
 
+# del log
 sudo truncate -s 0 /var/lib/docker/*/*/*-json.log
