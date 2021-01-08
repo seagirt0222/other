81d948fae49f1d9a23fbb2c5dff967124c779f21
seagirt0222 <43135436+seagirt0222@users.noreply.github.com>
2021-01-08 10:37:55 +0800

    Update install_case.sh

--- install_case.sh
+++ install_case.sh
@@ -4,16 +4,20 @@ echo -n "Chiose："
 read ANS
 
 case $ANS in
+# git kafka
   1) git clone https://github.com/seagirt0222/kafka-docker
      git clone https://github.com/deviantony/docker-elk
     echo "Git clone Kafka+Elk OK";;
-
+    
+# git grafana
   2) git clone https://github.com/seagirt0222/grafana
     echo "Git clone Grafana OK ";;
 
+# git tick
   3) git clone https://github.com/seagirt0222/tick
     echo "Git clone Tick OK";;
     
+#  install telegraf   
   4) wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 source /etc/lsb-release
 echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
@@ -23,7 +27,8 @@ sudo apt-get update && sudo apt-get install telegraf
 sudo systemctl start telegraf
  
 echo "install telegraf";;
-    
+
+# install filebeat
   5) wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sleep 3
@@ -32,6 +37,7 @@ sudo apt update
 sudo apt install filebeat
     echo "install filebeat";;
     
+# install docker & docker-compose     
   6) sudo timedatectl set-timezone Asia/Taipei
   
 sudo apt-get update 
@@ -60,6 +66,7 @@ fi
 
 echo "Install Docker-compose OK";;
     
+# change docker folder     
   7) sudo docker info | grep "Docker Root Dir"
 service docker stop
 sleep 2
@@ -82,13 +89,15 @@ service docker start
 sudo docker info | grep "Docker Root Dir"
 sleep 3
 echo "Change Docker Path Sussus & Please restart the device " && exit 0  ;;
-    
+ 
+# docker sudo  
    8) #sudo groupadd docker
 sudo gpasswd -a ${USER} docker
 sudo service docker restart
 newgrp - docker
 echo "docker don't use sudo OK ";;
 
+#install SSL 
    9) sudo apt-get update
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository universe
@@ -99,5 +108,5 @@ sudo apt-get install certbot python3-certbot-nginx -y
 sudo certbot --nginx
 echo "Letsencrypt SSL install OK";;
   *)
-   echo "只能按1,2,3,4,5,6,7,8的按鍵";;
+   echo "input number key[1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9]";;
 esac
